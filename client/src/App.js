9ee27cc0428ccc20f771d56171338fa106f0f04b
AmySultan <86156296+AmySultan@users.noreply.github.com>
2021-08-19 22:50:19 +0800

    Milestone 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,108 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
+  let [input, setInput] = useState({});
   let [students, setStudents] = useState([]);
+  // let [firstName, setFirstName] = useState("");
+  // let [lastName, setLastName] = useState("");
 
   useEffect(() => {
-    getStudents();
+    fetch("/students")
+      .then(res => res.json())
+      .then(json => {
+        setStudents(json);
+        console.log(json);
+      })
+      .catch(error => {});
   }, []);
 
-  const getStudents = () => {
-    fetch('/students')
+  const handleChange = e => {
+    console.log(e);
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addStudent();
+  };
+
+  const addStudent = id => {
+    fetch("/students", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(input)
+    })
       .then(response => response.json())
       .then(students => {
         setStudents(students);
+        console.log(students);
       })
       .catch(error => {
         console.log(error);
       });
-  }
+  };
+
+  const deleteStudent = id => {
+    // delete task from database
+    // upon success, update tasks
+    // upon failure, show error message
+    fetch("/students", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      // upon success, update tasks
+      .then(response => response.json())
+      .then(data => {
+        setStudents(students.filter(e => e.id !== id)); //takes from getAllStudents
+
+        // Continue fetch request here
+      })
+      // upon failure, show error message
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="App">
       <h1>CodeOp's Facebook</h1>
+      <form onSubmit={e => handleSubmit(e)}>
+        <label>
+          First name:
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            onChange={e => handleChange(e)}
+          ></input>
+        </label>
+        Last name:
+        <input
+          type="text"
+          name="lastname"
+          id="lastname"
+          onChange={e => handleChange(e)}
+        ></input>
+        <label for="lastname"></label>
+        <button type="submit">Submit</button>
+      </form>
+
+      <ul>
+        {students.map(item => (
+          <li key={item.id}>
+            {item.firstname} {item.lastname}
+            <button className="delete" onClick={e => deleteStudent(item.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- routes/students.js
+++ routes/students.js
@@ -2,8 +2,18 @@ var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
 
+const getAllStudents = (req, res) => {
+  db("SELECT * FROM students ORDER BY id ASC;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+};
+
+console.log("Before request");
 // GET student list
 router.get("/", function(req, res, next) {
+  // console.log("I'm here");
   db("SELECT * FROM students;")
     .then(results => {
       res.send(results.data);
@@ -14,16 +24,33 @@ router.get("/", function(req, res, next) {
 // GET one student
 router.get("/:id", function(req, res, next) {
   //your code here
+  db(`SELECT * from students where id =${req.params.id};`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
 });
 
 // INSERT a new student into the DB
 router.post("/", function(req, res, next) {
   //your code here
+  db(
+    `INSERT INTO students(firstname,lastname)VALUES ("${req.body.firstname}","${req.body.lastname}")`
+  )
+    .then(results => {
+      getAllStudents(req, res);
+    })
+    .catch(err => res.status(404).send(err));
 });
 
 // DELETE a student from the DB
 router.delete("/:id", function(req, res, next) {
   //your code here
+  db(`DELETE FROM students WHERE id = ${req.params.id};`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
 });
 
 module.exports = router;
